fd4981e79a6cdbaf14b772839d9cee37e165a6c4
Alex Medinsh <alex.medinsh@gmail.com>
2020-08-27 19:02:33 +0300

    Page Titles

--- js/mapTranslations.js
+++ js/mapTranslations.js
@@ -2,18 +2,15 @@ const fs = require('fs');
 
 const translations = require('./translations.json');
 
-const mapped = {
+const mapped = Object.entries(translations).reduce((ac,[key, values]) => {
+    Object.entries(values).forEach(([lang, text]) => {
+        ac[lang][key] = text;
+    });
+    return ac;
+}, {
     "en": {},
     "lv": {},
     "ru": {}
-}
-
-Object.entries(translations).forEach(([key, values])=>{
-    Object.entries(values).forEach(([lang, text])=>{
-        mapped[lang][key] = text;
-        
-    });
-    console.log('Mapped', key);
 });
 
 fs.writeFileSync('mapped.json', JSON.stringify(mapped, null, 2));
